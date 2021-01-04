79c6e63a3bd28f5102cccd2069b39f1f544ccf61
amine-nejjar <nejjar.ahmedamine@gmail.com>
2021-01-04 14:54:07 +0100

    modified bottom tabs

--- src/screens/homeClient/index.js
+++ src/screens/homeClient/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Icon} from 'react-native-elements'
 import HomeStack from './home'
 import ProfileStack from './profile'
 import ReservationStack from './reservations'
@@ -8,9 +9,18 @@ const Tab = createBottomTabNavigator();
 function HomeTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="HomeStack" component={HomeStack} />
-      <Tab.Screen name="ProfileStack" component={ProfileStack} />
-      <Tab.Screen name="ReservationStack" component={ReservationStack} />
+      <Tab.Screen name="HomeStack" component={HomeStack} options={{tabBarLabel: 'accueil',tabBarIcon: ({ color, size }) => (
+            <Icon name="home" type="antdesign" color={color} size={size} />
+          )
+        }}/>
+      <Tab.Screen name="ProfileStack" component={ProfileStack} options={{tabBarLabel: 'accueil',tabBarIcon: ({ color, size }) => (
+            <Icon name="user" type="antdesign" color={color} size={size} />
+          )
+        }}/>
+      <Tab.Screen name="ReservationStack" component={ReservationStack} options={{tabBarLabel: 'profile',tabBarIcon: ({ color, size }) => (
+            <Icon name="calendar" type="antdesign" color={color} size={size} />
+          )
+        }}/>
     </Tab.Navigator>
   );
 }
