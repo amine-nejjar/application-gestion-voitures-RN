35130fac2bc1ecf5b241e2861e9fd8952775796f
Manel Zengouri <manel95zengouri@gmail.com>
2021-02-09 04:45:33 +0100

    commit add car for Agence

--- src/screens/homeAgence/home/AddCar.js
+++ src/screens/homeAgence/home/AddCar.js
@@ -1,50 +1,100 @@
 import React from 'react'
-import {View, Text, StyleSheet , FlatList } from 'react-native'
+import {View, Text, StyleSheet , TouchableOpacity , Image , Alert} from 'react-native'
 import Header from '../../../shared/header'
 import * as firebase from 'firebase'
 import {Button} from 'react-native-elements'
 import {Input} from 'react-native-elements'
-
+import * as ImagePicker from 'expo-image-picker';
+import { MaterialIcons } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler'
  
 class AddCar extends React.Component{
     constructor(){ 
         super();
-        this.state={nom:"",email:"",prix:"",type:"",km:"",couleur:"",}
+        const user = firebase.auth().currentUser;
+        this.state={nom:"",email:"",prix:"",type:"",km:"",couleur:"", user :user.uid , url : ''}
+         console.log(this.state.user)
+        
     }
     
-    getCars = () => {
-        this.setState({isLoading:true})
-        firebase.database().ref('/cars').on('value', doc => {
-            var carArray = [];
-            doc.forEach(function(snap) {
-                var item = snap.val();
-                item.key = snap.key;
-                carArray.push(item);
-            });
-            this.setState({cars:carArray,isLoading:false})
-        })
-    }
+    onChooseImagePress = async (key) => {
 
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+        });
+        if (!result.cancelled) {
+
+            this.uploadImage(result.uri,key)
+                .then(() => {
+                    Alert.alert("Successfully uploaded");
+                }).then(() => {
+                   return firebase.storage().ref().child(key).getDownloadURL(); 
+                }).then(downloadURL => {
+                   console.log(`Successfully uploaded file and got download link - ${downloadURL}`);
+                   this.setState({
+                      url : downloadURL ,
+                      avatar : result.uri,
+                   });
+            
+                }).catch((error) => {
+                    Alert.alert('Error:', error.message);
+                });
+        }
+    }
+    uploadImage = async (uri, imageName) => {
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        var ref = firebase.storage().ref().child(imageName);
+        return ref.put(blob);
+
+        
+    }
+    AddInformations(){
+        var pushedRef = firebase.database().ref().child('cars').push(
+            {
+              agence : this.state.user ,
+              nom : this.state.nom ,
+              color  : this.state.couleur,
+              type : this.state.type,
+              km : this.state.km,
+              prix : this.state.prix ,
+              image : this.state.url ,
+
+            }
+        );
+        console.log(pushedRef.key);
+        var pushedRef = firebase.database().ref().child('cars').child(pushedRef.key).child('photos').push( this.state.url);
+    }
     render(){
         return (
             <View style={styles.container}>
             <Header navigation={this.props.navigation} title="Ajouter une voiture" />
-                <View style={styles.container2}>
-                    <Input style={styles.textInput} placeholder="Nom de voiture" onChangeText={(txt)=>this.setState({nom:txt})}/>
-                    <Input style={styles.textInput} placeholder="Prix de location" onChangeText={(txt)=>this.setState({prix:txt})}/>
-                    <Input style={styles.textInput} placeholder="Type de voiture" onChangeText={(txt)=>this.setState({type:txt})}/>
-                    <Input style={styles.textInput} placeholder="Kilometrage" onChangeText={(txt)=>this.setState({km:txt})}/>
-                    <Input style={styles.textInput} placeholder="Couleur" onChangeText={(txt)=>this.setState({couleur:txt})}/>
-                    
-                    <Button 
-                        title="Ajouter" 
-                        type='outline' 
-                        titleStyle={{ color : 'green'}}
-                        onPress={()=> console.log(this.state.cars)}         
-                        buttonStyle={{ borderColor : 'green'}}
-                    />
-                       
-                </View>                      
+                <ScrollView style={{ margin : '3%' }}>
+                    <View style={{  alignItems: 'center', width: "100%",justifyContent: 'center' }} >
+                        <TouchableOpacity style={styles.avatarPlaceholder} onPress={() => { this.onChooseImagePress(firebase.auth().currentUser.uid);}} >
+                            <Image source={{ uri: this.state.avatar }} style={styles.avatar}/>
+                                <MaterialIcons name="add-a-photo" size={30} color="white" />
+                        </TouchableOpacity>
+                    </View> 
+                    <View style={styles.container2}>
+                        <Input style={styles.textInput} placeholder="Nom de voiture" onChangeText={(txt)=>this.setState({nom:txt})}/>
+                        <Input style={styles.textInput} placeholder="Prix de location" onChangeText={(txt)=>this.setState({prix:txt})}/>
+                        <Input style={styles.textInput} placeholder="Type de voiture" onChangeText={(txt)=>this.setState({type:txt})}/>
+                        <Input style={styles.textInput} placeholder="Kilometrage" onChangeText={(txt)=>this.setState({km:txt})}/>
+                        <Input style={styles.textInput} placeholder="Couleur" onChangeText={(txt)=>this.setState({couleur:txt})}/>
+                        
+                        <Button 
+                            title="Ajouter" 
+                            type='outline' 
+                            titleStyle={{ color : 'white'}}
+                            onPress={()=> this.AddInformations()}         
+                            buttonStyle={{ backgroundColor : '#0ad86e'}}
+                        />
+                        
+                    </View>  
+                </ScrollView>                    
             </View>
             
           );
@@ -68,7 +118,24 @@ const styles = StyleSheet.create({
      separator: {
         marginBottom : 25,
     },
-    
+    avatarPlaceholder: {
+        width: 135,
+        height: 135,
+        backgroundColor: "grey",
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    avatar: {
+        position: 'absolute',
+        width: 130,
+        height: 130,
+        opacity: 0.6,
+        backgroundColor: "grey",
+        justifyContent: "center",
+        borderRadius: 100,
+        alignItems: "center",
+    }
 
 });
 export default AddCar
